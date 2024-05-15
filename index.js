5579e8616a83d06eb2be6723e6cfa539977ef9a5
Sabaris <sabaris.elango@gmail.com>
2024-05-16 00:15:43 +0530

    Knight travialis project mod

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // source - a fellow odin project coder - helped me understand the problem
-
+// knight game
 function Node(pos, path) {
   if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) {
     return null;
